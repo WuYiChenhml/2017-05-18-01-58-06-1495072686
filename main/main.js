f7ba0b5956c6d7432d9af2d427c25e60a65dc2c3
WuYiChenhml <1055200502@qq.com>
2017-07-12 08:52:45 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,3 +1,12 @@
-module.exports = function main() {
+module.exports = function main(string) {
   // Write your cade here
+  var length = string.length;
+  var num = Number(string);
+  var result = 0;
+  for(var i = length - 1; i < 0; i--)
+  {
+    result = result + Math.floor(num / Math.pow(10, i));
+    num = num - Math.floor(num / Math.pow(10, i)) * Math.pow(10, i);
+  }
+  return result;
 };
